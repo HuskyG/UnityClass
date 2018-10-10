3eea603174d67a73fc0cb38e839ed1bdc9590c03
GraceXia <xiaziyue@gmail.com>
2018-10-10 09:20:31 -0400

    October 10th
    bouncing balls and spinning torus

---
+++ SpaceLoop/Assets/Scripts/Loop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour {
+    public GameObject objPreFab;
+    GameObject objInstance;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("ObjFactory", 0f, 3f);
+    }
+
+    void ObjFactory()
+    {
+        objInstance = Instantiate(objPreFab, transform.position, transform.rotation);
+        Destroy(objInstance, 10f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
